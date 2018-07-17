ba0c16073f037e0b0c1972bb87c2bc33ad5c7b9b
Ghensys Valero <ghensysv@gmail.com>
2018-07-17 15:45:54 -0400

    Actualizacion del controlador, descomentado el proceso de registro

--- app/controlador/registrar_novedad.php
+++ app/controlador/registrar_novedad.php
@@ -5,12 +5,8 @@ require_once '../modelo/novedad.php';
 $tipo_novedad = $_POST['tipo_novedad'];
 $descripcion_novedad = $_POST['descripcion_novedad'];
 
-
-
 $novedad = new Novedad();
 $data = $novedad->tipoNovedad($tipo_novedad);
-
-//echo "tipo novedad";
 
 $fetch_tipo_novedad = pg_fetch_array($data);
 
@@ -18,14 +14,10 @@ if ($fetch_tipo_novedad['id_seguimiento'] == 1)
 {
 	$estatus = 1;
 }
-elseif ($fetch_tipo_novedad['id_seguimiento'] == 2) {
-	# code...
-}
+elseif ($fetch_tipo_novedad['id_seguimiento'] == 0)
 {
 	$estatus = 0;
 }
-
-//echo "asignacion de estatus";
 
 $data = array(
 	'tipo_novedad' => $tipo_novedad,
@@ -33,12 +25,7 @@ $data = array(
 	'estatus_novedad' => $estatus,
 	 );
 
-print_r($data);
-
-echo $estatus;
-//echo "creacion de array data";
-
-/*$novedad = new Novedad();
+$novedad = new Novedad();
 $registro = $novedad->registrarNovedad($data);
 
 $mensaje = "Novedad registrada satisfactoriamente.";
@@ -53,4 +40,4 @@ else
 {
 	echo "<script>alert('$error')</script>";
 	echo "<script>window.location.replace('../vista/novedad.php');</script>";
-}*/
+}
