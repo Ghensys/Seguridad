18351c8a449c4872dcd6b50ed9ddf103945aae15
Ghensys Valero <grvalero@bancoex.gob.ve>
2018-09-11 11:44:56 -0400

    Avance del mantenimiento de Novedades
    Controller y View para la administración de las novedades

--- app/modelo/novedad.php
+++ app/modelo/novedad.php
@@ -7,7 +7,7 @@ require_once 'conexion.php';
  */
 class Novedad
 {
-	
+
 	private $tipo_novedad;
 	private $descripcion_novedad;
 
@@ -22,7 +22,7 @@ class Novedad
 		$con = new Conexion();
 		$con->Conectar();
 
-		$sql = "SELECT 
+		$sql = "SELECT
 				id_seguimiento
 				FROM tipo_novedad
 				WHERE id_tipo_novedad = '$tipo_novedad';";
@@ -44,7 +44,7 @@ class Novedad
 		$con = new Conexion();
 		$con->Conectar();
 
-		
+
 
 		$sql = sprintf("INSERT INTO public.novedad(id_tipo_novedad, descripcion_novedad, estatus_novedad)
 				VALUES ('%s', '%s','%s')",
@@ -69,13 +69,13 @@ class Novedad
 		$con = new Conexion();
 		$con->Conectar();
 
-		$sql = "SELECT 
+		$sql = "SELECT
 				novedad.id_novedad,
 				novedad.descripcion_novedad,
 				novedad.fecha_novedad,
 				tipo_novedad.descripcion_tipo_novedad
 				FROM novedad, tipo_novedad
-				WHERE novedad.id_tipo_novedad = tipo_novedad.id_tipo_novedad 
+				WHERE novedad.id_tipo_novedad = tipo_novedad.id_tipo_novedad
 				AND
 				novedad.fecha_novedad BETWEEN '$fecha_consulta 00:00:00' AND '$fecha_consulta 23:59:59';";
 
@@ -97,6 +97,32 @@ class Novedad
 		$con->Conectar();
 
 		$sql = "SELECT * FROM novedad JOIN tipo_novedad ON tipo_novedad.id_tipo_novedad = novedad.id_tipo_novedad WHERE novedad.estatus_novedad = 1 ORDER BY novedad.fecha_novedad DESC;";
+
+		$query = pg_query($sql);
+
+		if ($query)
+		{
+			return $query;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public function ListaNovedad()
+	{
+		$con = new Conexion();
+		$con->Conectar();
+
+		$sql = "SELECT
+				tipo_novedad.id_tipo_novedad,
+				tipo_novedad.descripcion_tipo_novedad,
+				tipo_novedad.id_seguimiento,
+				tipo_novedad.estatus_dato,
+				estatus_datos.descripcion_estatus_dato
+				FROM tipo_novedad, estatus_datos
+				WHERE tipo_novedad.estatus_dato = estatus_datos.id ORDER BY descripcion_tipo_novedad ASC;";
 
 		$query = pg_query($sql);
 
